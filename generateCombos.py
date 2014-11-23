7c17cb98eb59e6930d7adc1f997875c959b1e543
sayemmh <sayemmh@gmail.com>
2014-11-22 20:29:12 -0500

    Creating new "groups" table
    Creating groups table with the two columns: group number and wID

--- generateCombos.py
+++ generateCombos.py
@@ -1,15 +1,13 @@
 import sqlite3 as lite
-import sys
-import glob
-import os.path
 import random
-from itertools import combinations
 
-con = lite.connect('/Users/Scott/Research/2211 S2014/AnonymousS2014Campus.sqlite')
+con = lite.connect('AnonymousS2014Campus.sqlite')
 cur = con.cursor()
 aList = []
 bList = []
-cur.execute('select wID from submissions where labNumber = 1 and url is not null')
+cur.execute('''select wID from submissions
+                WHERE labNumber = 1 AND url is not null''')
+
 data = cur.fetchall()
 n = 0
 cList = []
@@ -22,7 +20,17 @@ while len(bList) < 10000:
     if cList not in bList:
         bList.append(cList);
     cList = [];
+    
+cur.execute('''CREATE TABLE IF NOT EXISTS groups (groupnum int, wID text)''')
+#for j in bList
+#    wid1 = str(x[0])
+#    cur.execute('''INSERT INTO groups
+#                    VALUES(?,?)''',
+#                    [bList.index,wid1]);
+
 print(len(data))
 print (len(aList))       
 print(bList)
 print(len(bList))
+
+
